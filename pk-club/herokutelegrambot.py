0a4753568170ac34639fdfb680e66c943601346c
prabhakaran2304 <prabhavarul@gmail.com>
2021-10-05 17:48:48 +0530

    Update herokutelegrambot.py

--- pk-club/herokutelegrambot.py
+++ pk-club/herokutelegrambot.py
@@ -2,7 +2,6 @@
 Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
 Deployed using heroku.
 
-Author: liuhh02 https://medium.com/@liuhh02
 """
 
 import logging
@@ -15,7 +14,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'YOURTELEGRAMBOTTOKEN'
+TOKEN = '1463083442:AAGMyuH31Oeq1B1zml_gLSy8RN-qr427NiM'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -59,7 +58,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://pk-club.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
